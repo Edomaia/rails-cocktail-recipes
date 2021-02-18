70b848e894e67e0401ba209d30428fc0dcfdac40
edoardo maraia <edoardo.maraia@gmail.com>
2021-02-18 11:55:23 +0100

    created controller and respective actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/new'
+  get 'cocktails/create'
   root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create]
 end
