1500805ea85c4f82befefe1e321721826bdc4f4f
edoardo maraia <edoardo.maraia@gmail.com>
2021-02-18 15:49:23 +0100

    seeded application with cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,30 @@
 require 'open-uri'
 
-puts "Destroy ingredients"
+puts "Destroying reviews.."
+Review.destroy_all if Rails.env.development?
+
+puts "Destroying doses.."
+Dose.destroy_all if Rails.env.development?
+
+puts "Destroying ingredients.."
 Ingredient.destroy_all if Rails.env.development?
 
-puts "Destroy Cocktails"
+puts "Destroying Cocktails.."
 Cocktail.destroy_all if Rails.env.development?
 
-puts "Create ingredients"
+puts "Creating ingredients.."
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url).read)
 ingredients["drinks"].each do |ingredient|
   i = Ingredient.create(name: ingredient["strIngredient1"])
   puts "create #{i.name}"
+end
+
+puts "Creating Cocktails"
+
+5.times do |c|
+  Cocktail.create([{ name: "Espresso Martini"},
+                   { name: "Boulevardier"},
+                   { name: "Peach Please"},
+                   { name: "Negroni"}])
 end
