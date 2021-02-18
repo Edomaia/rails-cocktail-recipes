e3aea6690220402237ab098e4aa0149e098fddee
edoardo maraia <edoardo.maraia@gmail.com>
2021-02-18 13:46:42 +0100

    created routes for doses controller generated doses controller updated cocktails show action created new view for doses and rendered it on show page and displayed doses on cocktail show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/new'
-  get 'cocktails/create'
   root to: "cocktails#index"
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
